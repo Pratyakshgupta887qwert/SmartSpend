8d6d911ea47115a52635a3e2dd5a0875e0932ce1
Neeti Sharma <netzie9@gmail.com>
2026-02-17 10:11:04 +0530

    frontend-Login

--- SmartSpend.Web/src/App.jsx
+++ SmartSpend.Web/src/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
 
+const Dashboard=()=>{
+  return <h1>Dashboard</h1>
+};
 function App() {
-  
-
   return (
-    <>
-      <Sidebar />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
