ba9ac12d5799e941121612eaf54f707556c407f7
Neeti Sharma <netzie9@gmail.com>
2026-02-17 12:19:52 +0530

    try

--- SmartSpend.Web/src/App.jsx
+++ SmartSpend.Web/src/App.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 
-const Dashboard=()=>{
-  return <h1>Dashboard</h1>
-};
+import Dashboard from "./Pages/Dashboard";
+
+// const Dashboard=()=>{
+//   return <h1>Hii I amDashboard</h1>
+// };
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <>
+   
+    <Side />
+     <Router>
+       <Routes>
+         <Route path="/" element={<Login />} />
+         <Route path="/dashboard" element={<Dashboard />} />
+   
+       </Routes>
+     </Router>
+     </>
+     
   );
 }
 
